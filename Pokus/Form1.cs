d7cce23033b47ddc72a7ed9627ccf439f3759b87
tomasvochyan <tomasvochyan@outlook.cz>
2020-11-16 12:53:32 +0100

    Sčítání done

--- Pokus/Form1.cs
+++ Pokus/Form1.cs
@@ -16,5 +16,14 @@ namespace Pokus
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            int c = a + b;
+            label1.Text = "Soucet cisel je: " + c.ToString();
+        }
     }
 }
